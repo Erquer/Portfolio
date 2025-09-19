0f1d91735a26991c3c3f75a278000926b9fce762
erquer <baryla181@gmail.com>
2025-09-19 08:16:10 +0200

    init

--- next.config.mjs
+++
@@ -1,8 +0,0 @@
-/** @type {import('next').NextConfig} */
-const nextConfig = {
-    basePath: '/Portfolio',
-    output: "export",  // <=== enables static exports
-    reactStrictMode: true,
-};
-
-export default nextConfig;
